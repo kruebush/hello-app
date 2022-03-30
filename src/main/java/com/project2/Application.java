51e94a9a1d7102b30752a6b9abb6bc6e0ad97aa8
Kami Ruebush <kami691@revature.net>
2022-03-30 12:10:01 -0500

    Kami adding first Jenkin

---
+++ src/main/java/com/project2/Application.java
@@ -0,0 +1,19 @@
+package com.project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	@GetMapping("/sayHello")
+	public String sayHello() {
+		return "Hello My Friend";
+	}
+}
